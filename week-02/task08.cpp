ed8205f6277bbe57ffa471d07d2f023c98d2a14c
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 20:57:38 +0500

    01
    Removed comment about sorting method.

--- week-02/task08.cpp
+++ week-02/task08.cpp
@@ -6,14 +6,13 @@ int main() {
     int size;
 
     cin >> size;
-    int x[1000]; // безопасно фиксируем размер
+    int x[1000];
 
     if (size >= 1 && size <= 1000) {
 
         for (int i = 0; i < size; i++)
             cin >> x[i];
 
-        // Сортировка по возрастанию (через стандартный алгоритм)
         sort(x, x + size);
 
         for (int i = 0; i < size; i++)
