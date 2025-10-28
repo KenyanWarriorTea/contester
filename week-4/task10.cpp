5dbefa3e08348d49782b9b590e4657d77cf9b5e4
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 15:38:14 +0500

    Add task10.cpp to count character occurrences

---
+++ week-4/task10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    char arr[100][200], ch[100];
+    int counts[100] = {0};
+
+    for(int i = 0; i < n; i++)
+        cin >> ch[i] >> arr[i];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; arr[i][j] != '\0'; j++) {
+            if(arr[i][j] == ch[i]) counts[i]++;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        cout << counts[i] << " " << ch[i] << " in " << arr[i] << endl;
+
+    return 0;
+}
