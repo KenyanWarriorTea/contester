0d23b50716b26d59984d9693e4b68f2d47ffe269
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 15:40:34 +0500

    Add program to convert lowercase to uppercase

---
+++ week-4/task12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char text[101];
+    cin >> text;
+
+    for(int i = 0; text[i] != '\0'; i++)
+        if(text[i] >= 'a' && text[i] <= 'z') text[i] -= 32;
+
+    cout << text;
+
+    return 0;
+}
