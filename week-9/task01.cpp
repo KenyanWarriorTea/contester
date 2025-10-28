8bddbcac25ef4ff1a5522063f305f6908e1fe4dd
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 18:09:59 +0500

    Update task01.cpp to handle string input
    Refactor function to use string input and search for a substring.

--- week-9/task01.cpp
+++ week-9/task01.cpp
@@ -1,42 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void func(char display[8][8]) {
-    int amoB = 0, amoW = 0, amoR = 0, amoG = 0;
-
-    for(int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++) {
-            switch(display[i][j]) {
-                case '0': amoB++; display[i][j] = '-'; break;
-                case '1': amoW++; display[i][j] = '-'; break;
-                case '2': amoR++; break;
-                case '3': amoG++; display[i][j] = '-'; break;
-                default: break;
-            }
-        }
-    }
-
-    if(amoB > 0 && amoW > 0 && amoR > 0 && amoG > 0) {
-        for(int i = 0; i < 8; i++) {
-            for(int j = 0; j < 8; j++)
-                cout << display[i][j];
-            cout << endl;
-        }
-
-        cout << amoB << " " << amoW << " " << amoR << " " << amoG;
-    } else {
-        cout << "BAD INPUT LIST";
-    }
-}
-
 int main() {
-    char display[8][8];
+    string str, needFind;
 
-    for(int i = 0; i < 8; i++)
-        cin >> display[i];
+    getline(cin, str);
+    cin >> needFind;
 
-    func(display);
+    size_t pos = str.find(needFind);
+
+    if(pos == 0)
+        cout << -1 << endl;
+    else
+        cout << pos << endl;
 
     return 0;
 }
