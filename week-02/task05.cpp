5a6a545b5b727c45d368094493121f3c04eeafd3
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 20:57:18 +0500

    01

--- week-02/task05.cpp
+++ week-02/task05.cpp
@@ -5,29 +5,24 @@ int main() {
     int n;
     cin >> n;
 
-    // Проверяем, что размер массива допустим
     if (n < 1 || n > 1000)
         return 0;
 
     int arr[1000];
     
-    // Ввод элементов массива
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    // Инициализация максимума минимально возможным значением
     int maxValue = -2147483648;
     int countMax = 0;
 
-    // Поиск максимального значения
     for (int i = 0; i < n; i++) {
         if (arr[i] > maxValue) {
             maxValue = arr[i];
         }
     }
 
-    // Подсчёт количества элементов, равных максимуму
     for (int i = 0; i < n; i++) {
         if (arr[i] == maxValue) {
             countMax++;
