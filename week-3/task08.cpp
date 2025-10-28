3d44a87bef21e23ec2d8970eea7c506d7403d9f1
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 15:23:15 +0500

    Add matrix sum calculation in task08.cpp
    Implement a program to calculate the sum of elements above the main diagonal of a square matrix.

---
+++ week-3/task08.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n >= 1 && n <= 100) {
+        int mat[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> mat[i][j];
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--)
+                sum += mat[i][j];
+        }
+
+        cout << sum;
+    }
+
+    return 0;
+}
