856c5c42e7a99896666d2ba6e04c0aa4bd8081f2
Beksultan Tokmeil <mctbekab@gmail.com>
2025-10-28 15:55:35 +0500

    Add permutation function for string input

---
+++ week-5/task07.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void permute(string str, int l, int r) {
+    if (l == r) {
+        cout << str << endl;
+    } else {
+        for (int i = l; i <= r; i++) {
+            swap(str[l], str[i]);
+            permute(str, l + 1, r);
+            swap(str[l], str[i]); // восстановление
+        }
+    }
+}
+
+int main() {
+    string str;
+    cin >> str;
+
+    int n = str.size();
+    permute(str, 0, n - 1);
+
+    return 0;
+}
